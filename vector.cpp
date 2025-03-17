bf381bf6806b8bb2a15e85edd4c19b34af996d2d
mishu-maisha <maishamubashshira01@gmail.com>
2025-03-17 10:49:01 +0600

    vector file

---
+++ vector.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main (){
+    vector<int>v1;
+    v1={1,2,3};
+    
+    for(int val: v1){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+  
+ //push_back() for adding value at last index
+ //pop _back() to delete the value at last index
+ //vecname.size() to calculate size
+ //vecname.capacity() to calculate capacity of
+ //vecname.front() to find value at front   
+ //vecname.back() to find value at back
+ //vecname.at(index) value at particulur index    
+    
+}
